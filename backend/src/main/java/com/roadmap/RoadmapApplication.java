1a9ded7a687073d68a436c6a341bc49b835a8906
swalihaja <swajamadar202@gmail.com>
2026-08-06 23:17:06 +0530

    Force allow circular references

--- backend/src/main/java/com/roadmap/RoadmapApplication.java
+++ backend/src/main/java/com/roadmap/RoadmapApplication.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 public class RoadmapApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(RoadmapApplication.class, args);
     }
